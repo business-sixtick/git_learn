09579f8d3f56aa777ab7d27c558cebd8144eb223
sixtick_sub3 <business4dyd@gmail.com>
2024-12-18 16:35:16 +0900

    스테틱 테스트트

--- lib/widgets/github_pages_page.dart
+++ lib/widgets/github_pages_page.dart
@@ -30,6 +30,9 @@ class GitHubPagesPage extends ConsumerWidget {
               subtitle: Text('상단 메뉴바에서 Settings 클릭'
               '\r\n이 후 좌측 Pages 클릭'
               '\r\nBranch 항목에서 브렌치를 고르고 index.html의 위치를 고른다. / 루트와 /docs 폴더를 고를 수 있다. '
+              '\r\n이 후 save 버튼을 눌러 저장한다.'
+              '\r\nurl 형식은 https://아이디.github.io/저장소명'
+              '\r\nhttps://business-sixtick.github.io/git_learn'
               ),
             ),
             ListTile(
