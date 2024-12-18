6a2e0b758d5ceaf89483df336ac24a2b9c58ef75
sixtick_sub3 <business4dyd@gmail.com>
2024-12-18 11:37:22 +0900

    .

--- lib/widgets/git_start_page.dart
+++ lib/widgets/git_start_page.dart
@@ -31,7 +31,7 @@ class GitStartPage extends ConsumerWidget {
     > git reset HEAD^     # 최근 커밋 되돌리기기, 또는 git reset --hart "커밋해시" 명령으로 특정 커밋으로 되돌린다. 
     > git commit -m "메시지"    # 스테이징한 파일들에 버전을 부여한다. -am 옵션은 스테이징을 포함한다.
     > git push    # 커밋한 버전을 원격저장소에 밀어 넣는다. 
-    > git log     # 커밋 기록을 보여준다
+    > git log     # 커밋 기록을 보여준다 --oneline 옵션을 붙이면 커밋당 한줄씩 보여준다.
     > git diff    # 변경사항 확인하기. 좀 어렵다. vscode 에서 확인하는게 좋겠다 
 '''),
         ));
